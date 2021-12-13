997228696ddad980e11762582f762fc20608228e
Anderson <and.santos123@gmail.com>
2021-12-13 10:03:08 -0300

    anotacoes

--- lib/demo_web/controllers/page_controller.ex
+++ lib/demo_web/controllers/page_controller.ex
@@ -15,10 +15,12 @@ defmodule DemoWeb.PageController do
   end
 
   def playground(conn, _params) do
-    headline = "This is a test headline" # A frase é a mesma do "inspect" porém com o estilo de codificação diferent
+
+    # variavel recebe dados
+    headline = "This is a test headline" # A frase é a mesma do "inspect" porém com o estilo de codificação diferente
     {:ok, timestamp} = DateTime.now("Etc/UTC")
 
-
+    # prepara variavel para enviar para modelo html
     conn
     |> assign(:headline, headline)
     |> assign(:timestamp, timestamp)
